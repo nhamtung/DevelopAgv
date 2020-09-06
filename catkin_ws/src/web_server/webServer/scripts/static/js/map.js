b8d12b7d36a449f2551e34806c60c83d69011a6c
nhamtung <nhamtung125@gmail.com>
2020-09-06 23:52:15 +0700

    zoom and pan Map

--- catkin_ws/src/web_server/webServer/scripts/static/js/map.js
+++ catkin_ws/src/web_server/webServer/scripts/static/js/map.js
@@ -12,8 +12,10 @@ var app = new Vue({
         mapGridClient: null,
         interval: null,
 
-        dataShow: 0,
+        dataShow1: 0,
+        dataShow2: 0,
     },
+
     // helper methods to connect to ROS
     methods: {
         connect: function () {
@@ -21,28 +23,143 @@ var app = new Vue({
             this.ros = new ROSLIB.Ros({
                 url: this.rosbridge_address
             })
+
+            // Setup mouse event handlers
+            // this.mouseDown = false
+            zoomKey = false
+            panKey = false
+            startPos = new ROSLIB.Vector3()
+            
+
             this.ros.on('connection', () => {
                 this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
                 this.connected = true
                 this.loading = false
 
-                this.mapViewer = new ROS2D.Viewer({
+                var mapViewer = new ROS2D.Viewer({
                     divID: 'map',
-                    width: 2024,
+                    width: 550,
                     height: 550
                 })
+
+                this.center = new ROSLIB.Vector3()
+                this.startShift = new ROSLIB.Vector3()
+                this.startScale = new ROSLIB.Vector3()
+                // Add zoom to the viewer.
+                var zoomView = new ROS2D.ZoomView({
+                    rootObject: mapViewer.scene
+                })
+                // Add panning to the viewer.
+                var panView = new ROS2D.PanView({
+                    rootObject: mapViewer.scene
+                })
+                    // Add planned path
+                // var plannedPath = new ROS2D.NavPath({
+                //     ros: this.ros,
+                //     rootObject: mapViewer.scene,
+                //     pathTopic: '/plan'
+                // })
+                    // Add robot pose and trace
+                // var robotTrace = new ROS2D.PoseAndTrace({
+                //     ros: this.ros,
+                //     rootObject: mapViewer.scene,
+                //     poseTopic: '/robot_pose',
+                //     withTrace: true,
+                //     maxTraceLength : 200
+                // })
+                    // Add navigRation goal
+                // var navGoal = new ROS2D.NavGoal({
+                //     ros: this.ros,
+                //     rootObject : mapViewer.scene,
+                //     actionTopic : '/move_base'
+                // })
 
                 // Setup the map client.
                 this.mapGridClient = new ROS2D.OccupancyGridClient({
                     ros: this.ros,
-                    rootObject: this.mapViewer.scene,
-                    continuous: true,
+                    rootObject: mapViewer.scene,
+                    continuous: true
                 })
+        
                 // Scale the canvas to fit to the map
                 this.mapGridClient.on('change', () => {
-                    this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
-                    this.mapViewer.shift(-10, this.mapGridClient.currentGrid.pose.position.y);
-                    this.dataShow = this.mapGridClient.currentGrid.pose.position.y
+                    gridWidth = this.mapGridClient.currentGrid.width/13;
+                    gridHeigh = this.mapGridClient.currentGrid.height/13;
+                    // this.dataShow1 = gridWidth;
+                    // this.dataShow2 = gridHeigh;
+                    mapViewer.scaleToDimensions(gridWidth, gridHeigh);
+
+                    grid_pose_x = this.mapGridClient.currentGrid.pose.position.x/16;
+                    grid_pose_y = this.mapGridClient.currentGrid.pose.position.y/8;
+                    // this.dataShow1 = grid_pose_x;
+                    // this.dataShow2 = grid_pose_y;
+                    mapViewer.shift(grid_pose_x, grid_pose_y);
+
+                    // plannedPath.initScale();
+                    // robotTrace.initScale();
+                    // navGoal.initScale();
+                    // registerMouseHandlers();
+                })
+
+                mapViewer.scene.addEventListener('stagemousedown', function(event) {
+                    if (event.nativeEvent.ctrlKey === true) {
+                        console.log("Ctrl + Mouse");
+                        zoomKey = true;
+                        zoomView.startZoom(event.stageX, event.stageY);
+                    }
+                    else if (event.nativeEvent.shiftKey === true) {
+                        console.log("Shift + Mouse");
+                        panKey = true;
+                        panView.startPan(event.stageX, event.stageY);
+                    }
+                    else {
+                        console.log("Mouse");
+                        var pos = mapViewer.scene.globalToRos(event.stageX, event.stageY);
+                        // navGoal.startGoalSelection(pos);
+                    }
+
+                    startPos.x = event.stageX;
+                    startPos.y = event.stageY;
+                    console.log("startPos.x: " + startPos.x);
+                    console.log("startPos.y: " + startPos.y);
+                    
+                    this.mouseDown = true;
+                })
+    
+                mapViewer.scene.addEventListener('stagemousemove', function(event) {
+                    if (this.mouseDown === true) {
+                        if (zoomKey === true) {
+                            var dy = event.stageY - startPos.y;
+                            var zoom = 1 + 10*Math.abs(dy) / mapViewer.scene.canvas.clientHeight;
+                            if (dy < 0)
+                                zoom = 1 / zoom;
+                            zoomView.zoom(zoom);
+                        }
+                        else if (panKey === true) {
+                            panView.pan(event.stageX, event.stageY);
+                        }
+                        else {
+                            var pos = mapViewer.scene.globalToRos(event.stageX, event.stageY);
+                            // navGoal.orientGoalSelection(pos);
+                        }
+                    }
+                })
+    
+                mapViewer.scene.addEventListener('stagemouseup', function(event) {
+                    if (this.mouseDown === true) {
+                        if (zoomKey === true) {
+                            zoomKey = false;
+                        }
+                        else if (panKey === true) {
+                            panKey = false;
+                        }
+                        else {
+                            var pos = mapViewer.scene.globalToRos(event.stageX, event.stageY);
+                            // var goalPose = navGoal.endGoalSelection(pos);
+                            // navGoal.sendGoal(goalPose);
+                        }
+                        this.mouseDown = false;
+                    }
                 })
 
             })
@@ -56,10 +173,13 @@ var app = new Vue({
                 document.getElementById('map').innerHTML = ''
             })
         },
+
         disconnect: function () {
             this.ros.close()
         },
+        
     },
+
     mounted() {
         this.interval = setInterval(() => {
             if (this.ros != null && this.ros.isConnected) {
